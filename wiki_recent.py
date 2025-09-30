f829b62a3b96e61831cc1be9a008bb8a6ce58a3a
housetroycys@gmail.com <housetroycys@gmail.com>
2025-09-30 14:05:05 -0400

    Add files via upload
    Iteration 1: Query wiki page for the 30 most recent changes to the page.

---
+++ wiki_recent.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import requests
+
+
+#WIKI API variable
+WIKIPEDIA_API = "https://en.wikipedia.org/w/api.php"
+
+def main(argv):
+    #An argument parser to the argument for the script that will be the wiki page title
+    parser = argparse.ArgumentParser(prog="wiki_recent")
+    parser.add_argument("title", help="Wikipedia article title")
+    args = parser.parse_args(argv)
+
+    #Query parameters for the wiki page
+    params = {
+        "action": "query",
+        "format": "json",
+        "prop": "revisions",
+        "titles": args.title,
+        "rvprop": "timestamp|user",
+        "rvlimit": "30",
+        "redirects": "1"
+    }
+
+    #http header
+    headers = {
+        "User-Agent": "WikiRecentEditsBot/1.0 (troy.house@bsu.edu)"
+    }
+
+    #making a request to the page
+    try:
+        response = requests.get(WIKIPEDIA_API, params=params, headers=headers, timeout=10)
+        response.raise_for_status()
+
+        # Print raw JSON
+        print(response.text)
+
+    #catches errors
+    except requests.RequestException as e:
+        print(f"Network error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+main(sys.argv[1:])
