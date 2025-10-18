ea48a4df3bfc830ccaab5eb4fef2175484c7088b
housetroycys@gmail.com <housetroycys@gmail.com>
2025-10-18 09:46:33 -0400

    First branch
    Gets User Input, JSON, and Parses

--- wiki_recent.py
+++ wiki_recent.py
@@ -26,17 +26,13 @@ def main():
     parser.add_argument("title", help="Wikipedia article title")
     args = parser.parse_args()
 
-    try:
-        parsed = get_wikipedia_data(args.title)
-        if "query" in parsed and "redirects" in parsed["query"]:
-            redirect_to = parsed["query"]["redirects"][0]["to"]
-            print(f"Redirected to {redirect_to}")
-        print(json.dumps(parsed, separators=(",", ":")))
-    except urllib.error.HTTPError as e:
-        print(f"HTTP error: {e.code} {e.reason}")
-    except urllib.error.URLError as e:
-        print(f"Network error: {e.reason}")
-       
+    
+    parsed = get_wikipedia_data(args.title)
+    if "query" in parsed and "redirects" in parsed["query"]:
+        redirect_to = parsed["query"]["redirects"][0]["to"]
+        print(f"Redirected to {redirect_to}")
+    print(json.dumps(parsed, separators=(",", ":")))
+    
+        
 if __name__ == "__main__":
     main()
-
